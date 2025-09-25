89c77b53c3af5122c82d15e00ab5e385df952366
Andrii Popesku <andrii.popesku@globalstaffingsupport.com>
2025-09-25 20:13:02 +0100

    Update image paths in Home component and configure basePath and assetPrefix in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "test-front";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
